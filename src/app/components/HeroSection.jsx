1f0f42dc446d651cb115901d403a2921d3235fa3
Anurag Gaikwad <gaikwadanurag16@gmail.com>
2025-07-05 16:28:48 +0530

    Update Hero Section. Fully Responsive

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,13 +7,13 @@ function HeroSection() {
    <div className="flex justify-center mt-10 mx-10 max-w-screen-xl md:mx-auto">
      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10 ">
       <div className="flex flex-col justify-center space-y-5 p-5">
-        <h1 className="font-[700] text-6xl text-[#1A2B4A] ">We don’t sell you agencies. We help you find yours</h1>
-        <h1 className="font-[400] text-xl text-[#4B5563] ">Answer a few simple questions - get matched with<br/>agencies who get your business and think like you do.</h1>
-        <div className="mt-5">
+        <h1 className="font-[700] text-3xl text-[#1A2B4A] sm:text-3xl lg:text-6xl ">We don’t sell you agencies. We help you find yours</h1>
+        <h1 className="font-[400] text-base lg:text-xl text-[#4B5563] ">Answer a few simple questions - get matched with agencies who get your business and think like you do.</h1>
+        <div className="space-y-5">
           <CTAButton 
           className=""
           title="Find Your Agency..." />
-          <p className="font-[400] text-xm mt-5 text-[#6B7280] ">Takes 2 minutes. No sales calls. No clutter.</p>
+          <p className="font-[400] text-xm mt-3 text-[#6B7280] ">Takes 2 minutes. No sales calls. No clutter.</p>
         </div>
       </div>
       <div  className="">
