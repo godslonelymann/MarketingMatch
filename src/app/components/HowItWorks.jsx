1785f18440b771c23c7b9a576e1ca8d92b94b9e5
Anurag Gaikwad <gaikwadanurag16@gmail.com>
2025-07-07 17:27:13 +0530

    How it Works structure done. Work on design and responsiveness

--- src/app/components/HowItWorks.jsx
+++ src/app/components/HowItWorks.jsx
@@ -6,19 +6,25 @@ function HowItWorks() {
   return (
     <div className="flex justify-center items-center bg-red-200 max-w-screen h-screen mt-10">
       
-        <div className="text-center ">
-          <h1>How it Works</h1>
-          <div className="flex space-x-10 mt-10">
+        <div className="bg-amber-50 w-[90%] text-center flex flex-col justify-center items-center h-[80%] m-auto">
+          <h1 className="font-[700] text-3xl">Let's make this easy.</h1>
+          <div className="flex  items-center  justify-around bg-amber-400 w-full mt-20">
             <div>
-              <HowItWorksIcon icon = ""/>
+              <HowItWorksIcon 
+              title = "Tell us about your business"
+              icon = "/icons/icon1.png"/>
               <h1>card 1</h1>
             </div>
             <div>
-              <HowItWorksIcon icon = "icons\i-1.png" />
+              <HowItWorksIcon 
+              title = "Set your style & mindset"
+              icon = "/icons/icon2.png" />
               <h1>card 1</h1>
             </div>
             <div>
-              <HowItWorksIcon icon = "icons/i-2.png" />
+              <HowItWorksIcon 
+              title = "Get 3 curated agency matches"
+              icon = "/icons/icon3.png" />
               <h1>card 1</h1>
             </div>
           </div>
--- src/app/components/ui/HowItWorksIcons.jsx
+++ src/app/components/ui/HowItWorksIcons.jsx
@@ -1,11 +1,27 @@
 import Image from 'next/image';
 
-function HowItWorksIcon({ icon }){
+function HowItWorksIcon({ title, icon }){
 
     return(
-        <div className="bg-[#2563EB] rounded-full h-[64px] w-[64px]">
-            {icon}
-        </div>
+        <>
+          <div className='flex justify-center items-center flex-col'>
+              <div className="bg-[#2563EB] rounded-full h-[64px] w-[64px] flex justify-center items-center ">
+                <img src={icon} alt="" />
+                {/* <div className='bg-amber-100'>
+                    <h1>{title}</h1>
+                </div> */}
+                
+            </div>
+            <div className='bg-amber-100'>
+                    <h1>{title}</h1>
+            </div>
+          
+          </div>
+        
+        </>
+        
+        
+
     );
 
 }
