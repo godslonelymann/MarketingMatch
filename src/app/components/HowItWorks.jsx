58c4fecd67cd31b7b145c174e67528e2fa145f7f
Anurag Gaikwad <gaikwadanurag16@gmail.com>
2025-07-08 15:34:22 +0530

    HowItWorks section done; responsiveness to be done

--- src/app/components/HowItWorks.jsx
+++ src/app/components/HowItWorks.jsx
@@ -4,28 +4,34 @@ import HowItWorksIcon from "./ui/HowItWorksIcons"
 
 function HowItWorks() {
   return (
-    <div className="flex justify-center items-center bg-red-200 max-w-screen h-screen mt-10">
+    <div className="flex justify-center items-center bg-[#F5F6FA] max-w-screen h-[90vh] mt-10">
       
-        <div className="bg-amber-50 w-[90%] text-center flex flex-col justify-center items-center h-[80%] m-auto">
-          <h1 className="font-[700] text-3xl">Let's make this easy.</h1>
-          <div className="flex  items-center  justify-around bg-amber-400 w-full mt-20">
+        <div className="w-[90%] text-center flex flex-col justify-center items-center h-[80%] m-auto max-w-screen-xl">
+          <h1 className="font-[700] text-3xl text-[#1A2B4A]">Let's make this easy.</h1>
+          <div className="flex  items-center  justify-around w-full mt-20">
             <div>
               <HowItWorksIcon 
               title = "Tell us about your business"
-              icon = "/icons/icon1.png"/>
-              <h1>card 1</h1>
+              icon = "/icons/icon1.png"
+              description = "industry, growth stage, goals"
+              />
+             
             </div>
             <div>
               <HowItWorksIcon 
               title = "Set your style & mindset"
-              icon = "/icons/icon2.png" />
-              <h1>card 1</h1>
+              icon = "/icons/icon2.png"
+              description = "bold vs steady, creative vs data"
+              />
+              
             </div>
             <div>
               <HowItWorksIcon 
               title = "Get 3 curated agency matches"
-              icon = "/icons/icon3.png" />
-              <h1>card 1</h1>
+              icon = "/icons/icon3.png" 
+              description = "with proof, vibes & past results"
+              />
+             
             </div>
           </div>
         </div>
--- src/app/components/ui/HowItWorksIcons.jsx
+++ src/app/components/ui/HowItWorksIcons.jsx
@@ -1,19 +1,18 @@
 import Image from 'next/image';
 
-function HowItWorksIcon({ title, icon }){
+function HowItWorksIcon({ title, icon, description }){
 
     return(
         <>
           <div className='flex justify-center items-center flex-col'>
               <div className="bg-[#2563EB] rounded-full h-[64px] w-[64px] flex justify-center items-center ">
                 <img src={icon} alt="" />
-                {/* <div className='bg-amber-100'>
-                    <h1>{title}</h1>
-                </div> */}
+               
                 
             </div>
-            <div className='bg-amber-100'>
-                    <h1>{title}</h1>
+            <div className="">
+                  <h1 className='text-[#2563EB] font-[700] text-xl mt-5'>{title}</h1>
+                  <p className='text-[#2563EB] font-[400] text-base mt-5'>{description}</p>
             </div>
           
           </div>
