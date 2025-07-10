f5372c8a3232dfbef950a3be8546622c7db587ce
Anurag Gaikwad <gaikwadanurag16@gmail.com>
2025-07-10 23:36:59 +0530

    Agency Types section structure done

--- src/app/components/ui/AgencyTypesCard.jsx
+++ src/app/components/ui/AgencyTypesCard.jsx
@@ -1,9 +1,9 @@
 function AgencyTypesCard({icon, title, description}){
 
     return(
-       <div className="bg-[#F5F6FA] h-[80vh] w-[260px] text-center">
-            <div>
-                <img src={icon} alt="" />
+       <div className="bg-[#F5F6FA]  w-[275px] text-center rounded-2xl ">
+            <div className="flex flex-col ">
+                <img src={icon} alt="" height={36} width={36} />
                 <h1>{title}</h1>
                 <p>{description}</p>
             </div>
