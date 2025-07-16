f781afce198a567cc0a30136c5c9a6983c0f70a8
Anurag Gaikwad <gaikwadanurag16@gmail.com>
2025-07-16 21:34:39 +0530

    changes

--- src/app/FindYourAgency/page.js
+++ src/app/FindYourAgency/page.js
@@ -6,7 +6,7 @@ function FindYourAgency() {
     <>
         <div className='h-screen w-full flex items-center flex-col'>
             <div className='h-1/4 w-full max-w-screen-xl flex justify-center items-center flex-col space-y-3'>
-                <h1 className='font-[500] text-5xl text-[#111827]'> Let's find an agency that gets you</h1>
+                <h1 className='font-[500] text-5xl text-[#111827]'>Lets find an agency that gets you</h1>
                 <p className='font-[400] text-xl text-[#4B5563] '>Answer a few quick questions. Takes 2 minutes.</p>
             </div>
             <Questions
